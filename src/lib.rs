ea3dd9c986ded1254e601ceb4177ac7b967281da
GithungoIan <githungoian53@gmail.com>
2021-09-04 13:27:15 +0300

    build thread pool

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-	#[test]
-	fn it_works() {
-		assert_eq!(2 + 2, 4)
+pub struct ThreadPool;
+
+impl ThreadPool {
+	pub fn new(size: usize) -> ThreadPool {
+		ThreadPool
+	}
+	pub fn execute<F>(&self, f: F)
+	where
+		F: FnOnce() + Send + 'static,
+	{
 	}
 }
